98225c76ea3d13eb53b54de7748aaa10575b71ce
Tushar Sukhwal <tushars7740@gmail.com>
2023-10-18 21:42:13 +0530

    new commit

--- Issues&TODO.md
+++ Issues&TODO.md
@@ -1,4 +1,4 @@
-TODO 
+# TODO 
 1. team -> getAllTeams (admin only )
 2. team -> deleteTeam (team Leader  only )
 3. team -> getMyTeam (team members only )
@@ -6,17 +6,17 @@ TODO
 
 4. competition -> updateCompetition 
 
+5. ~~update the googleauth to store the cookies in correct place . ~~
 
 
 
-
-Queries 
+# Queries 
 1. should one person be only in one team ? 
 
 2. 
 
 
-ISSUES (## -> resolevd)
+# ISSUES 
 1. Don't send otp to frontend in the response of auth/sendotp (the person can enter the otp without having to check the mail and can enter using wrong mail also cuz he is getting the otp in the broswer )
 
 2. auth/sendotp route is giving response as 200 even if the mail is not sent  (it should give 400)
--- controllers/auth/GoogleAuth.js
+++ controllers/auth/GoogleAuth.js
@@ -18,32 +18,55 @@ exports.googleSignIn = async (req, res) => {
       })
       .then(async (response) => {
         const email = response.data.email;
-        const existingUser = await User.findOne({ email , userType: "google"});
-        if (!existingUser)
+        const user = await User.findOne({ email, userType: "google" });
+        if (!user)
           return res.status(404).json({ message: "User doesn't exist!" });
 
-        const token = jwt.sign(
-          {
-            email: existingUser.email,
-            id: existingUser._id,
-          },
-          process.env.JWT_SECRET,
-          { expiresIn: "1h" }
-        );
-        const userResponse = {
-          firstName: existingUser.firstName,
-          lastName: existingUser.lastName,
-          email: existingUser.email,
-          id: existingUser._id,
-          token,
+        const playload = {
+          email: user.email,
+          id: user._id,
+          userType: user.userType,
         };
 
-        res.status(200).json({ result: userResponse });
+        const token = jwt.sign(playload, process.env.JWT_SECRET, {
+          expiresIn: "2h",
+        });
+        user.token = token;
+
+        const options = {
+          expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+          httpOnly: true,
+        };
+
+        res.cookie("token", token, options).status(200).json({
+          success: true,
+          token,
+          user,
+          message: "User logged in successfully",
+        });
+
+        // const token = jwt.sign(
+        //   {
+        //     email: existingUser.email,
+        //     id: existingUser._id,
+        //   },
+        //   process.env.JWT_SECRET,
+        //   { expiresIn: "1h" }
+        // );
+        // const userResponse = {
+        //   firstName: existingUser.firstName,
+        //   lastName: existingUser.lastName,
+        //   email: existingUser.email,
+        //   id: existingUser._id,
+        //   token,
+        // };
+
+        // res.status(200).json({ result: userResponse });
       })
       .catch((err) => {
         res.status(400).json({ message: "Invalid access token!" });
         console.log("error in signin controller");
-        console.log(err)
+        console.log(err);
       });
   } else {
     res.send("Invalid");
@@ -54,7 +77,7 @@ exports.googleSignUp = async (req, res) => {
   console.log("signupController");
   if (req.body.googleAccessToken) {
     const { googleAccessToken } = req.body;
-    console.log(googleAccessToken);
+    // console.log(googleAccessToken);
     axios
       .get("https://www.googleapis.com/oauth2/v3/userinfo", {
         headers: {
@@ -69,10 +92,14 @@ exports.googleSignUp = async (req, res) => {
 
         const existingUser = await User.findOne({ email, userType: "google" });
 
-        if (existingUser)
-          return res.status(400).json({ message: "User already exists!" });
+        console.log("hello");
 
-        const result = await User.create({
+        if (existingUser) {
+          return res.status(400).send(  "User already exists!" );
+          console.log("user alreday exists");
+        }
+
+        const user = await User.create({
           email,
           firstName,
           lastName,
@@ -80,26 +107,50 @@ exports.googleSignUp = async (req, res) => {
           image: picture,
         });
 
-        const token = jwt.sign(
-          {
-            email: result.email,
-            id: result._id,
-          },
-          process.env.JWT_SECRET,
-          { expiresIn: "1h" }
-        );
+        // const token = jwt.sign(
+        //   {
+        //     email: result.email,
+        //     id: result._id,
+        //   },
+        //   process.env.JWT_SECRET,
+        //   { expiresIn: "1h" }
+        // );
 
-        const userResponse = {
-          firstName: result.firstName,
-          lastName: result.lastName,
-          email: result.email,
-          token,
+        const playload = {
+          email: user.email,
+          id: user._id,
+          userType: user.userType,
         };
 
-        res.status(200).json({ result: userResponse });
+        const token = jwt.sign(playload, process.env.JWT_SECRET, {
+          expiresIn: "2h",
+        });
+        user.token = token;
+
+        console.log(token);
+
+        const options = {
+          expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+          httpOnly: true,
+        };
+
+        res.cookie("token", token, options).status(200).json({
+          success: true,
+          token,
+          user,
+          message: "User logged in successfully",
+        });
+        // const userResponse = {
+        //   firstName: result.firstName,
+        //   lastName: result.lastName,
+        //   email: result.email,
+        //   token,
+        // };
+
+        // res.status(200).json({ result: userResponse });
       })
       .catch((err) => {
-        res.status(400).json({ message: "Invalid access token!" });
+        res.status(400).send( "Invalid access token!" );
       });
   } else {
     console.log("Token not found");
