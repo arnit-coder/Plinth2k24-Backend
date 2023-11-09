906a3b73c1ee0e11bb02e943473101049a307cb2
AdityaTheJaat <programmingID1306@gamil.com>
2023-11-09 17:09:10 +0530

    otp email

--- controllers/mailSender.js
+++ controllers/mailSender.js
@@ -5,7 +5,9 @@ const mailSender = async (email, title, body) => {
   try {
     //Creating a transporter
     let transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
